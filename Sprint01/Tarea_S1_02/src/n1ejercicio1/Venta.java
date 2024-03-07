cf672f80b8a9d5fd944b4be9258ad565f0e08355
Leandro Pereyra <leandro-pereyra@hotmail.es>
2024-03-07 17:11:14 +0100

    Tarea_S1_02: Nivel 1 - Ejercicio 2 - ArrayIndexOutOfBoundsException

--- Sprint01/Tarea_S1_02/src/n1ejercicio1/Venta.java
+++ Sprint01/Tarea_S1_02/src/n1ejercicio1/Venta.java
@@ -9,6 +9,7 @@ public class Venta {
     private double total_venta = 0;
 
     public double calcularTotal() throws VentaBuidaException {
+
         try {
             if(productos.length == 0) {
                 throw new VentaBuidaException("Para hacer una venta primero debes añadir productos");
@@ -18,10 +19,16 @@ public class Venta {
                 total_venta += producto.getPrecio();
             }
 
+            Producto lanzarExcepción_OutOfBounds = productos[-1];
+
         } catch (VentaBuidaException e) {
             String message = e.getMessage();
 
             System.out.println(message);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("Excepción ArrayIndexOutOfBoundsException: " + e.getMessage());
         }
 
         return total_venta;
