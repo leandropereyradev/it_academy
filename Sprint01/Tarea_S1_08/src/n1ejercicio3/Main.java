e2c8ccf809ef427f17cbe896c9e2446af565c898
Leandro Pereyra <leandro-pereyra@hotmail.es>
2024-03-16 18:41:22 +0100

    Tarea_S1_08: Nivel 1 - Ejercicio 3

---
+++ Sprint01/Tarea_S1_08/src/n1ejercicio3/Main.java
@@ -0,0 +1,13 @@
+package n1ejercicio3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> meses = Arrays.asList("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                                            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+
+        meses.forEach(mes -> System.out.println(mes));
+    }
+}
