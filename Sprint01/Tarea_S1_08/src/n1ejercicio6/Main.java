fe7270cb25bc642b86a599f806588872f1d72090
Leandro Pereyra <leandro-pereyra@hotmail.es>
2024-03-16 19:31:18 +0100

    Tarea_S1_08: Nivel 1 - Ejercicio 6

---
+++ Sprint01/Tarea_S1_08/src/n1ejercicio6/Main.java
@@ -0,0 +1,19 @@
+package n1ejercicio6;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Object> lista = Arrays.asList("Enero", 1, "Febrero", "Marzo", "Abril", 2, "Mayo", "Junio",
+                "Julio", 3, "Agosto", "Septiembre", 2.68, "Octubre", "Noviembre", 0.78, "Diciembre");
+
+        lista.stream()
+                .filter(elemento -> elemento instanceof String)
+                .map(String.class :: cast)
+                .sorted(Comparator.comparing(String :: length))
+                .forEach(System.out :: println);
+    }
+}
